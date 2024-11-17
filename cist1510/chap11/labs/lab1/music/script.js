730409d5a8e9f5e712f38fde6f8565463096ce4c
Chumiwumi <118708347+Chumiwumi@users.noreply.github.com>
2024-11-17 15:01:40 -0500

    Add files via upload

---
+++ cist1510/chap11/labs/lab1/music/script.js
@@ -0,0 +1,15 @@
+/*
+Author: Zuleyma Cruz Duarte
+Date: 09/11/24
+File Name: script.js
+*/
+
+//Hamburger menu function
+function hamburger() {
+var menu = document.getElementById("menu-links");
+if (menu.style.display === "block") {
+menu.style.display = "none";
+} else {
+menu.style.display = "block";
+}
+}
